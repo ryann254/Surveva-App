319090cb7f53ae4cfa37943e99b9696fd0050053
ryann254 <keddelyronjoz@gmail.com>
2024-10-08 19:15:03 +0300

    feat: add commentsModal ui

--- lib/pages/VotingPage.dart
+++ lib/pages/VotingPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:surveva_app/widgets/discoveryWidgets.dart';
 
 class VotingPage extends StatefulWidget {
@@ -16,11 +15,32 @@ class _VotingPageState extends State<VotingPage>
     with SingleTickerProviderStateMixin {
   final profileImg = '';
   bool isLiked = false;
+  String selectedAnswer = '';
   late AnimationController _animationController;
   late Animation<double> _animation;
   List<int> genderAnalytics = [70, 27, 3];
   List<int> ageAnalytics = [60, 25, 15];
   List<int> geoAnalytics = [40, 35, 25, 5];
+  List<Map<String, dynamic>> comments = [
+    {
+      'username': 'Roronoa Zoro',
+      'profileImg': '',
+      'comment': 'The king of hell has arrived',
+      'date': '2024-02-01',
+    },
+    {
+      'username': 'Monkey D. Luffy',
+      'profileImg': '',
+      'comment': 'I am the future king of the pirates',
+      'date': '2024-02-01',
+    },
+    {
+      'username': 'Sanji',
+      'profileImg': '',
+      'comment': 'I will find the all blue',
+      'date': '2024-02-01',
+    }
+  ];
 
   @override
   void initState() {
@@ -112,7 +132,9 @@ class _VotingPageState extends State<VotingPage>
                                   style: TextStyle(
                                       fontSize: 14,
                                       fontWeight: FontWeight.w500,
-                                      color: Theme.of(context).colorScheme.secondary),
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
                                 ),
                                 const SizedBox(width: 10),
                                 Text(
@@ -120,7 +142,9 @@ class _VotingPageState extends State<VotingPage>
                                   style: TextStyle(
                                       fontSize: 14,
                                       fontWeight: FontWeight.w500,
-                                      color: Theme.of(context).colorScheme.secondary),
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
                                 ),
                               ],
                             ),
@@ -136,8 +160,8 @@ class _VotingPageState extends State<VotingPage>
                       ),
                       Text(
                         widget.question['question'],
-                        style:
-                            const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
                         textAlign: TextAlign.center,
                       ),
                       const SizedBox(height: 24),
@@ -146,43 +170,64 @@ class _VotingPageState extends State<VotingPage>
                         physics: const NeverScrollableScrollPhysics(),
                         itemCount: widget.question['answers'].length,
                         separatorBuilder: (context, index) => Container(
-                          margin: const EdgeInsets.symmetric(vertical: 12.5),
                           height: 1,
                           width: double.infinity,
                           color: Colors.white,
                         ),
                         itemBuilder: (context, index) {
                           final answer = widget.question['answers'][index];
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                answer,
-                                style: const TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500),
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedAnswer = answer;
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(12.5),
+                              decoration: BoxDecoration(
+                              color: selectedAnswer == answer
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Theme.of(context).colorScheme.surface,
                               ),
-                              Container(
-                                width: 24,
-                                height: 24,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(24),
-                                  border: Border.all(
-                                      color: const Color(0xffDDDAD3), width: 1),
-                                ),
-                              )
-                            ],
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    answer,
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  Container(
+                                    width: 24,
+                                    height: 24,
+                                    decoration: BoxDecoration(
+                                      color: selectedAnswer == answer
+                                          ? Theme.of(context).primaryColor
+                                          : Theme.of(context)
+                                              .colorScheme
+                                              .surface,
+                                      borderRadius: BorderRadius.circular(24),
+                                      border: Border.all(
+                                          color: const Color(0xffDDDAD3),
+                                          width: 1),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
                           );
                         },
                       ),
                       Container(
-                        margin: const EdgeInsets.symmetric(vertical: 12.5),
                         height: 1,
                         width: double.infinity,
                         color: Colors.white,
                       ),
                       const SizedBox(
-                        height: 14,
+                        height: 28,
                       ),
                       GestureDetector(
                         onTap: () {},
@@ -197,7 +242,8 @@ class _VotingPageState extends State<VotingPage>
                               child: Text(
                                 'Submit',
                                 style: TextStyle(
-                                    color: Theme.of(context).colorScheme.onPrimary,
+                                    color:
+                                        Theme.of(context).colorScheme.onPrimary,
                                     fontSize: 16,
                                     fontWeight: FontWeight.w500),
                               ),
@@ -216,36 +262,50 @@ class _VotingPageState extends State<VotingPage>
                       const SizedBox(
                         height: 18,
                       ),
-                      genderAndAgeAnalytics(context, _animation, genderAnalytics, true),
+                      genderAndAgeAnalytics(
+                          context, _animation, genderAnalytics, true),
                       const SizedBox(height: 24),
-                      genderAndAgeAnalytics(context, _animation, ageAnalytics, false),
+                      genderAndAgeAnalytics(
+                          context, _animation, ageAnalytics, false),
                       const SizedBox(height: 24),
                       geographyAnalytics(context, _animation, geoAnalytics),
-                      
+                      const SizedBox(height: 12),
                     ],
                   ),
                 ),
               ),
             ),
-          Padding(
-            padding:  const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
-            child:  Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isLiked = !isLiked;
-                    });
-                  },
-                  child: Icon(
-                    Icons.favorite,
-                    size: 36,
-                    color: isLiked ? Colors.red : Theme.of(context).colorScheme.secondary,)),
-                SvgPicture.asset('assets/discovery/comments.svg', height: 36, width: 36)
-              ],
-            ),
-          )
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 16.0, right: 16.0, top: 8.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isLiked = !isLiked;
+                        });
+                      },
+                      child: Icon(
+                        Icons.favorite,
+                        size: 36,
+                        color: isLiked
+                            ? Colors.red
+                            : Theme.of(context).colorScheme.secondary,
+                      )),
+                  GestureDetector(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                commentsModal(context, comments));
+                      },
+                      child: SvgPicture.asset('assets/discovery/comments.svg',
+                          height: 36, width: 36))
+                ],
+              ),
+            )
           ],
         ),
       ),
