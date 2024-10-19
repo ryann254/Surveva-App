e6338b3030381430e21ae94bd3c7e0105dad5be5
ryann254 <keddelyronjoz@gmail.com>
2024-10-19 09:37:47 +0300

    feat: add ReportPage ui

--- lib/pages/profile/ReportPage.dart
+++ lib/pages/profile/ReportPage.dart
@@ -13,7 +13,101 @@ class _ReportPageState extends State<ReportPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(context),
-      body: SafeArea(child: Column(children: [],)),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 37),
+                      const Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              'With you, we become better!',
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'If you encounter any issues using the app or find any errors, please describe them, and we will do our best to assist you as quickly as possible.',
+                              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      SizedBox(
+                        height: 222,
+                        child: Stack(
+                          children: [
+                            TextField(
+                              maxLines: 8,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+                                  borderRadius: BorderRadius.circular(24)
+                                ),
+                                hintText: 'Describe the problem',
+                                hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Theme.of(context).colorScheme.secondary)
+                              ),
+                            ),
+                            Positioned(
+                              left: 8,
+                              bottom: 16,
+                              child: GestureDetector(
+                                onTap: () {
+                                  // Add attachment logic here
+                                },
+                                child: Icon(
+                                  Icons.attach_file,
+                                  color: Theme.of(context).colorScheme.secondary,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, left: 16.0, right: 16.0, bottom: 16.0),
+              child: GestureDetector(
+                onTap: () {
+                  // Add your onTap logic here
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Send',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
